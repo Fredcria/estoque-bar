50790e36a1a730086e6bfa86d907aadd6e72149d
Fredcria <139786596+Fredcria@users.noreply.github.com>
2026-06-26 11:10:27 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+var CACHE = 'estoque-bar-v2';
+var ASSETS = ['./', './index.html', './app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  if(e.request.url.indexOf('script.google.com')>=0){
+    e.respondWith(fetch(e.request));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(function(cached){
+    return cached || fetch(e.request).then(function(res){
+      var clone=res.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request,clone);});
+      return res;
+    });
+  }));
+});
